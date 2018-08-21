bb751f475420418680763756ccd16a775452fc33
Vladislav Sabanov <vladislav.sabanov@gmail.com>
2018-08-21 21:24:39 +0500

    Refactor better

--- src/Summoner/CLI.hs
+++ src/Summoner/CLI.hs
@@ -118,14 +118,14 @@ versionP = infoOption summonerVersion
    <> help "Show summoner's version"
 
 summonerVersion :: String
-summonerVersion = toString $ unlines $ map toText [sVersion, sHash, sDate, sDirty]
+summonerVersion = toString $ intercalate "\n" [sVersion, sHash, sDate, sDirty]
   where
-    sVersion = blueCode <> boldCode <> "Summoner version: " <> resetCode <>  showVersion version
-    sHash = blueCode <> boldCode <> "➤ Git revision: " <> resetCode <> $(gitHash)
-    sDate = blueCode <> boldCode <> "➤ Commit date: " <> resetCode <> $(gitCommitDate)
+    sVersion = blueCode <> boldCode <> "Summoner " <> "v" <>  showVersion version <> resetCode
+    sHash = " ➤ " <> blueCode <> boldCode <> "Git revision: " <> resetCode <> $(gitHash)
+    sDate = " ➤ " <> blueCode <> boldCode <> "Commit date:  " <> resetCode <> $(gitCommitDate)
     sDirty = if $(gitDirty)
         then redCode <> "There are non-committed files." <> resetCode
-        else ""
+        else mempty
 
 -- All possible commands.
 summonerP :: Parser Command
