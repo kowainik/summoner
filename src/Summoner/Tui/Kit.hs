c8e02690383f59dd36e72ec2d963a46e416a8945
Dmitry Kovanikov <kovanikov@gmail.com>
2018-11-18 18:20:05 +0800

    Add User form

--- src/Summoner/Tui/Kit.hs
+++ src/Summoner/Tui/Kit.hs
@@ -14,39 +14,50 @@ module Summoner.Tui.Kit
        , initialSummonKit
 
          -- * Lenses
-       , userL
-       , test1L
+         -- ** SummonKit
+       , user
        , tools
+
+         -- ** User
+       , owner
+       , fullName
+       , email
        ) where
 
 import Lens.Micro.TH (makeFields)
 
 import Summoner.Tui.CheckBox (CheckBox, toCheckBoxes)
-import Summoner.Tui.EditField (EditField, toEditFields)
 
-
-data Test1 = Test1 Text Text
-    deriving (Show)
 
 -- | Global TUI state.
 data SummonKit = SummonKit
-    { summonKitUserL  :: [EditField]
-    , summonKitTest1L :: [CheckBox Test1]
-    , summonKitTools  :: [CheckBox Tool]
+    { summonKitUser  :: User
+    , summonKitTools :: [CheckBox Tool]
+    } deriving (Show)
+
+data User = User
+    { userOwner    :: Text
+    , userFullName :: Text
+    , userEmail    :: Text
     } deriving (Show)
 
 -- | Represents the build tool that can be used in the generated project.
 data Tool
     = Cabal
     | Stack
-  deriving (Show)
+    deriving (Show)
 
-makeFields ''SummonKit
 
 -- | Initial global state of the tui.
 initialSummonKit :: SummonKit
 initialSummonKit = SummonKit
-    { summonKitUserL  = toEditFields [ "Owner", "Full name", "Email"]
-    , summonKitTest1L = toCheckBoxes [Test1 "a" "a", Test1 "b" "b"]
-    , summonKitTools  = toCheckBoxes [Cabal, Stack]
+    { summonKitUser  = User
+        { userOwner = ""
+        , userFullName = ""
+        , userEmail = ""
+        }
+    , summonKitTools = toCheckBoxes [Cabal, Stack]
     }
+
+makeFields ''SummonKit
+makeFields ''User
