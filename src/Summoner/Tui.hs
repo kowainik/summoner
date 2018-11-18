3e5cb2ed2cd224002585a27b36d757195efc85e2
Dmitry Kovanikov <kovanikov@gmail.com>
2018-11-18 20:55:37 +0800

    Add GitHub form

--- src/Summoner/Tui.hs
+++ src/Summoner/Tui.hs
@@ -13,13 +13,14 @@ import Brick (App (..), AttrMap, BrickEvent (VtyEvent), Padding (Pad), Widget, a
               customMain, hBox, halt, padTop, str, vBox, vLimit, (<+>), (<=>))
 import Brick.Focus (focusRingCursor)
 import Brick.Forms (Form, checkboxField, editTextField, focusedFormInputAttr, formFocus, formState,
-                    handleFormEvent, invalidFormInputAttr, listField, newForm, renderForm,
-                    setFormConcat, (@@=))
+                    handleFormEvent, invalidFormInputAttr, listField, newForm, radioField,
+                    renderForm, setFieldConcat, setFormConcat, (@@=))
 
 import Summoner.License (LicenseName)
 import Summoner.Tui.GroupBorder (groupBorder, (|>))
-import Summoner.Tui.Kit (SummonKit (..), cabal, category, desc, email, fullName, initialSummonKit,
-                         maybeLicense, owner, project, repo, stack, user)
+import Summoner.Tui.Kit (SummonKit (..), appVeyor, cabal, category, desc, email, enabled, fullName,
+                         gitHub, initialSummonKit, maybeLicense, owner, private, project, repo,
+                         stack, travis, user)
 
 import qualified Brick (on)
 import qualified Brick.Widgets.Border as B
@@ -37,20 +38,35 @@ summonTui = do
 
 
 data SummonForm
+    -- User
     = UserOwner
     | UserFullName
     | UserEmail
+
+    -- Project
     | ProjectName
     | ProjectDesc
     | ProjectCat
     | ProjectLicense
+
+    -- Build tools
     | CabalField
     | StackField
+
+     -- Build tools
+    | ToolsBox Int
+
+      -- github fields
+    | GitHubEnable
+    | GitHubDisable
+    | GitHubPrivate
+    | GitHubTravis
+    | GitHubAppVeyor
     deriving (Eq, Ord, Show)
 
 -- Creates the inout form from the given initial 'TreasureChest'.
 mkForm :: forall e . SummonKit -> Form SummonKit e SummonForm
-mkForm sk@SummonKit{..} = setFormConcat myBox $ newForm
+mkForm = setFormConcat arrangeColumns . newForm
     ( groupBorder "User"
         [ 2 |> label "Owner" @@= editTextField (user . owner) UserOwner (Just 1)
         , 1 |> label "Full name" @@= editTextField (user . fullName) UserFullName (Just 1)
@@ -67,7 +83,16 @@ mkForm sk@SummonKit{..} = setFormConcat myBox $ newForm
         [ 2 |> checkboxField cabal CabalField "Cabal"
         , 2 |> checkboxField stack StackField "Stack"
         ]
-    ) sk
+   ++ groupBorder "GitHub"
+        [ 2 |> setFieldConcat hBox . radioField (gitHub . enabled)
+            [ (True, GitHubEnable, "Enable")
+            , (False, GitHubDisable, "Disable")
+            ]
+        , 1 |> checkboxField (gitHub . private)  GitHubPrivate  "Private"
+        , 1 |> checkboxField (gitHub . travis)   GitHubTravis   "Travis"
+        , 2 |> checkboxField (gitHub . appVeyor) GitHubAppVeyor "AppVeyor"
+        ]
+    )
   where
     label :: String -> Widget n -> Widget n
     label s w = str s <+>  w
@@ -75,9 +100,14 @@ mkForm sk@SummonKit{..} = setFormConcat myBox $ newForm
     widgetList :: Bool -> LicenseName -> Widget SummonForm
     widgetList p l = C.hCenter $ if p then str ("[" ++ show l ++ "]") else str $ show l
 
-    myBox :: [Widget SummonForm] -> Widget SummonForm
-    myBox ws = let (a, b) = splitAt 7 ws in
-        hBox [vBox a, vBox b]
+    arrangeColumns :: [Widget SummonForm] -> Widget SummonForm
+    arrangeColumns widgets =
+        let (column1, columns23) = splitAt 7 widgets in
+        let (column2, column3)   = splitAt 2 columns23 in
+        hBox [ vBox column1
+             , vBox column2
+             , vBox column3
+             ]
 
 theMap :: AttrMap
 theMap = attrMap V.defAttr
@@ -86,7 +116,7 @@ theMap = attrMap V.defAttr
     , (invalidFormInputAttr, V.white `Brick.on` V.red)
     , (focusedFormInputAttr, V.black `Brick.on` V.yellow)
     , (L.listAttr,           V.white `Brick.on` V.blue)
-    , (L.listSelectedAttr,   V.blue `Brick.on` V.white)
+    , (L.listSelectedAttr,   V.blue  `Brick.on` V.white)
     ]
 
 draw :: Form SummonKit e SummonForm -> [Widget SummonForm]
