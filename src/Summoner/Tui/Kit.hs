dfa71dd0aeb93bf7c23463c0b543cadbff03389d
Veronika Romashkina <vrom911@gmail.com>
2018-11-18 17:31:18 +0800

    Basic layout for TUI (#209)
    * Basic layout for TUI
    
    * Fix hlint

---
+++ src/Summoner/Tui/Kit.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE FlexibleInstances      #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses  #-}
+{-# LANGUAGE TemplateHaskell        #-}
+
+{- | This module contains data types to work with application form.
+'SummonKit' is the data type containing the values manipulated by the fields
+in the form.
+-}
+
+module Summoner.Tui.Kit
+       ( -- * Data types
+         SummonKit (..)
+       , initialSummonKit
+
+         -- * Lenses
+       , test1L
+       , test2L
+       ) where
+
+import Lens.Micro.TH (makeFields)
+
+import Summoner.Tui.CheckBox (CheckBox, toCheckBoxes)
+
+
+data Test1 = Test1 Text Text
+    deriving (Show)
+data Test2 = Test2 Text Int
+    deriving (Show)
+
+-- | Global TUI state.
+data SummonKit = SummonKit
+    { summonKitTest1L :: [CheckBox Test1]
+    , summonKitTest2L :: [CheckBox Test2]
+    } deriving (Show)
+
+makeFields ''SummonKit
+
+-- | Initial global state of the tui.
+initialSummonKit :: SummonKit
+initialSummonKit = SummonKit
+    { summonKitTest1L = toCheckBoxes [Test1 "a" "a", Test1 "b" "b"]
+    , summonKitTest2L = toCheckBoxes [Test2 "a2" 1, Test2 "b2" 2]
+    }
