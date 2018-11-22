356b22e1cf061570f9d2db0ae7ed71ba82dda5cd
Dmitry Kovanikov <kovanikov@gmail.com>
2018-11-23 03:02:35 +0800

    Add CLI to summon-tui

--- src/Summoner/Tui/Widget.hs
+++ src/Summoner/Tui/Widget.hs
@@ -5,10 +5,13 @@ module Summoner.Tui.Widget
        ( label
        , borderLabel
        , hArrange
+       , listInBorder
        ) where
 
-import Brick (Padding (Pad), Widget, hBox, padRight, str, (<+>))
+import Brick (Padding (Pad), Widget, padRight, (<+>))
 import Brick.Widgets.Border (borderWithLabel)
+import Brick.Widgets.Center (center)
+import Brick.Widgets.Core (hBox, hLimit, str, txtWrap, vBox, vLimit, withAttr)
 
 
 -- | Adds label to the Form's field.
@@ -33,3 +36,31 @@ hArrange = hBox . updateHead (padRight (Pad 2))
     updateHead :: (a -> a) -> [a] -> [a]
     updateHead _ []       = []
     updateHead f (a : as) = f a : as
+
+{- | Shows list of elements inside 'borderWithLabel'.
+
+__Example:__
+@
+┌────────────Some label──────────────────┐
+│                                        │
+│          > 8.2.2                       │
+│          > 8.4.3                       │
+│          > 8.4.4                       │
+│                                        │
+└────────────────────────────────────────┘
+@
+
+-}
+listInBorder
+    :: String  -- ^ Border label
+    -> Int     -- ^ Horizontal limit
+    -> Int     -- ^ Additional vertical limit
+    -> [Text]  -- ^ List of text entries
+    -> Widget n
+listInBorder name limitH extraLimitV list = center
+    $ hLimit limitH
+    $ vLimit (length list + 4 + extraLimitV)
+    $ borderLabel name
+    $ center
+    $ vBox
+    $ map (withAttr "blue-fg" . txtWrap . ("➤ " <>)) list
