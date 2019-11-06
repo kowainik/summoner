9c3d6286d23ba855d553cc08c68d98a2d3355a08
Dmitrii Kovanikov <kovanikov@gmail.com>
2019-11-06 14:31:35 +0300

    [#367] Add `Raw` constructor to `Source` data type
    Resolves #367

--- summoner-cli/src/Summoner/Source.hs
+++ summoner-cli/src/Summoner/Source.hs
@@ -20,19 +20,31 @@ import qualified Toml
 
 -- | Type of the source resource.
 data Source
-    -- | URL link to the source file.
+    {- | URL link to the source file. Such files will be downloaded by URL. But
+    they are ingored in the @offline@ mode.
+    -}
     = Url !Text
-    -- | File path to the local source file.
+
+    {- | File path to the local source file.
+    -}
     | File !FilePath
-    -- | Link to external file.
+
+    {- | Link to external file. Generated file contains some short text and a
+    a give url link.
+    -}
     | Link !Text
+
+    {- | Raw file text content.
+    -}
+    | Raw !Text
     deriving stock (Show, Eq)
 
 showSource :: Source -> Text
 showSource = \case
-    Url _  -> "Url"
+    Url  _ -> "Url"
     File _ -> "File"
     Link _ -> "Link"
+    Raw  _ -> "Raw"
 
 -- TODO: return Maybe
 matchUrl :: Source -> Either TomlBiMapError Text
@@ -49,11 +61,17 @@ matchLink :: Source -> Either TomlBiMapError Text
 matchLink (Link link) = Right link
 matchLink e           = Left $ WrongConstructor "Link" $ showSource e
 
+-- TODO: return Maybe
+matchRaw :: Source -> Either TomlBiMapError Text
+matchRaw (Raw raw) = Right raw
+matchRaw e         = Left $ WrongConstructor "Raw" $ showSource e
+
 -- DEPRECATED: To be removed in 2.0
 sourceT :: Key -> TomlCodec Source
 sourceT nm = Toml.match (_Url  >>> Toml._Text)   (nm <> "url")
          <|> Toml.match (_File >>> Toml._String) (nm <> "file")
          <|> Toml.match (_Link >>> Toml._Text)   (nm <> "link")
+         <|> Toml.match (_Raw  >>> Toml._Text)   (nm <> "raw")
   where
     _Url :: TomlBiMap Source Text
     _Url = Toml.prism Url matchUrl
@@ -64,6 +82,9 @@ sourceT nm = Toml.match (_Url  >>> Toml._Text)   (nm <> "url")
     _Link :: TomlBiMap Source Text
     _Link = Toml.prism Link matchLink
 
+    _Raw :: TomlBiMap Source Text
+    _Raw = Toml.prism Raw matchRaw
+
 {- | This 'TomlCodec' is used in the @files@ field of config. It decodes
 corresponding constructor from the top-level key.
 -}
@@ -72,15 +93,17 @@ sourceCodec = asum
     [ Toml.dimatch (rightToMaybe . matchUrl) Url (Toml.text "url")
     , Toml.dimatch (rightToMaybe . matchFile) File (Toml.string "file")
     , Toml.dimatch (rightToMaybe . matchLink) Link (Toml.text "link")
+    , Toml.dimatch (rightToMaybe . matchRaw) Raw (Toml.text "raw")
     ]
 
 fetchSource :: Bool -> Source -> IO (Maybe Text)
 fetchSource isOffline = \case
     File path -> catch (Just <$> readFileText path) (fileError path)
-    Url url   -> if isOffline
+    Url url -> if isOffline
         then Nothing <$ infoMessage ("Ignoring fetching from URL in offline mode from source: " <> url)
         else fetchUrl url `catch` urlError url
     Link link -> putLink link
+    Raw raw -> pure $ Just raw
   where
     fileError :: FilePath -> SomeException -> IO (Maybe Text)
     fileError path _ = errorMessage ("Couldn't read file: " <> toText path)
