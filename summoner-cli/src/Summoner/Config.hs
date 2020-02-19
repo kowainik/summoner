a08c7adb917be8b1fef608b31fd6fd83bcf0daa7
Dmitrii Kovanikov <kovanikov@gmail.com>
2020-02-19 19:48:31 +0000

    Make HLint happy

--- summoner-cli/src/Summoner/Config.hs
+++ summoner-cli/src/Summoner/Config.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DataKinds            #-}
-{-# LANGUAGE DeriveAnyClass       #-}
 {-# LANGUAGE FlexibleContexts     #-}
 {-# LANGUAGE FlexibleInstances    #-}
 {-# LANGUAGE StandaloneDeriving   #-}
