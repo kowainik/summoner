f69d1aa95f33a3ffc4c111dc1049d92136e31e25
Veronika Romashkina <vrom911@gmail.com>
2020-06-29 11:14:44 +0100

    Test scripts with the default GHC

--- summoner-cli/test/Test/Script.hs
+++ summoner-cli/test/Test/Script.hs
@@ -7,7 +7,7 @@ module Test.Script
 import NeatInterpolation (text)
 import Test.Hspec (Spec, describe, it, shouldBe)
 
-import Summoner.GhcVer (GhcVer (..))
+import Summoner.Default (defaultGHC)
 import Summoner.Settings (Tool (..))
 import Summoner.Template.Script (scriptFile)
 
@@ -15,16 +15,16 @@ import Summoner.Template.Script (scriptFile)
 scriptSpec :: Spec
 scriptSpec = describe "script golden tests" $ do
     it "correctly creates cabal script" $
-        scriptFile Ghc844 Cabal `shouldBe` cabalScript
+        scriptFile defaultGHC Cabal `shouldBe` cabalScript
     it "correctly creates stack script" $
-        scriptFile Ghc865 Stack `shouldBe` stackScript
+        scriptFile defaultGHC Stack `shouldBe` stackScript
 
 cabalScript :: Text
 cabalScript = [text|
 #!/usr/bin/env cabal
 {- cabal:
 build-depends:
-  , base ^>= 4.11.1.0
+  , base ^>= 4.14.0.0
 -}
 
 main :: IO ()
@@ -35,7 +35,7 @@ stackScript :: Text
 stackScript = [text|
 #!/usr/bin/env stack
 {- stack
-  --resolver lts-14.27
+  --resolver nightly-2020-06-29
   script
   --package base
 -}
